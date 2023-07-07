5a7b1524025961e7c4786acee4e4031c71b3eb4a
Justin Linton <127802772+aberrator9@users.noreply.github.com>
2023-07-07 15:08:31 -0400

    Simplify, extend theme swapping.

--- theme.js
+++ theme.js
@@ -1,32 +1,7 @@
 ﻿const darkModeToggle = document.getElementById("dark-mode-toggle");
-let darkBG = "#2b2b2b";
-let darkText = "#e7e7e7";
-let lightBG = "#e9ddc6";
-let lightText = "#2b2b2b";
 
-function init() {
-    if (!localStorage.getItem("darkMode")) {
-        localStorage.setItem("darkMode", "true");
-    }
-    setTheme(false);
+function toggleTheme() {
+    document.body.classList.toggle('dark-theme')
 }
 
-function setTheme(toggle) {
-    let darkMode = JSON.parse(localStorage.getItem("darkMode"));
-    if (toggle) {
-        darkMode = !darkMode;
-        localStorage.setItem("darkMode", JSON.stringify(darkMode));
-    }
-
-    if (darkMode) {
-        document.body.style.backgroundColor = darkBG;
-        document.body.style.color = darkText;
-    } else {
-        document.body.style.backgroundColor = lightBG;
-        document.body.style.color = lightText;
-    }
-}
-
-darkModeToggle.onclick = () => setTheme(true);
-
-init();
+darkModeToggle.onclick = () => toggleTheme(true);
