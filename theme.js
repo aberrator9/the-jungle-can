3689611b742d789873907e45bd8a4262633696c5
Justin Linton <127802772+aberrator9@users.noreply.github.com>
2023-07-08 21:04:39 -0400

    Fix bug: html not resizing correctly in portrait mode, due to parent div set to 'position:absolute'.

--- theme.js
+++ theme.js
@@ -1,7 +1,13 @@
-﻿const darkModeToggle = document.getElementById("dark-mode-toggle");
+﻿const themeToggle = document.getElementById("theme-toggle");
+const svgs = document.querySelectorAll("svg");
 
 function toggleTheme() {
-    document.body.classList.toggle('dark-theme')
+    document.body.classList.toggle('dark-theme');
+
+    // SVGs need updated manually
+    for (let i = 0; i < svgs.length; i++) {
+        svgs[i].classList.toggle('dark-theme');
+    }
 }
 
-darkModeToggle.onclick = () => toggleTheme(true);
+themeToggle.onclick = toggleTheme;
