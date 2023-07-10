96d273c965f9a5dbcbb3848bc43eb123a0cb1543
Justin Linton <127802772+aberrator9@users.noreply.github.com>
2023-07-10 13:24:39 -0400

    Correct css syntax to allow svg color change on theme toggle.

--- theme.js
+++ theme.js
@@ -3,11 +3,6 @@ const svgs = document.querySelectorAll("svg");
 
 function toggleTheme() {
     document.body.classList.toggle('dark-theme');
-
-    // SVGs need updated manually
-    for (let i = 0; i < svgs.length; i++) {
-        svgs[i].classList.toggle('dark-theme');
-    }
 }
 
 themeToggle.onclick = toggleTheme;
