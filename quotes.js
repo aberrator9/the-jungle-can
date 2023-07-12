624e2a099da93b552359340b9f15510938281966
Justin Linton <127802772+aberrator9@users.noreply.github.com>
2023-07-11 22:35:47 -0400

    Add click to generate prompt.

--- quotes.js
+++ quotes.js
@@ -1,7 +1,8 @@
-const jungleQuote = document.getElementById("jungle-quote");
-const happenQuote = document.getElementById("happen-quote");
-const wonderlandQuote = document.getElementById("wonderland-quote");
-const generateButton = document.getElementById("generate-button");
+const jungleQuote = document.getElementById('jungle-quote');
+const happenQuote = document.getElementById('happen-quote');
+const wonderlandQuote = document.getElementById('wonderland-quote');
+const generateButton = document.getElementById('generate-button');
+const prompt = document.getElementById('prompt');
 
 function init() {
     if (localStorage.quote1 === undefined) {
@@ -21,6 +22,9 @@ async function getQuotes() {
         localStorage.quote1 = jungleQuote.innerHTML = data.jungle[Math.floor(Math.random() * data.jungle.length)];;
         localStorage.quote2 = happenQuote.innerHTML = data.happen[Math.floor(Math.random() * data.happen.length)];
         localStorage.quote3 = wonderlandQuote.innerHTML = data.wonderland[Math.floor(Math.random() * data.wonderland.length)];
+
+        prompt.style.display = 'none';
+        scrollToTop();
     } catch (e) {
         console.log('Error:', e);
         throw e;
@@ -35,6 +39,6 @@ function scrollToTop() {
     });
 }
 
-generateButton.onclick = getQuotes && scrollToTop;
+generateButton.onclick = getQuotes;
 
 init();
